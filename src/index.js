9769a25fe8fa88903409af64fef43e2b3f714298
tjohns96 <trevorjohnson9601@gmail.com>
2022-08-02 16:20:15 -0400

    Commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Button variant = 'contained'>Hello</Button>
+    <Button variant = 'contained'>Hellos</Button>
   </React.StrictMode>
 );
 
